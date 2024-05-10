2f39ee3eefc4bccd18669039b0245f7069131a17
Hawkeyecoder4 <162034853+Hawkeyecoder4@users.noreply.github.com>
2024-05-10 23:27:47 +0500

    Add files via upload

---
+++ Practice JS/index.js
@@ -0,0 +1,70 @@
+// let name = "John Doe";
+//  age = "30" ;
+// isStudent = true;
+
+// console.log(name);
+// console.log(age);
+// console.log(isStudent);
+ 
+// console.log (typeof name);
+// console.log (typeof age);
+// console.log (typeof isStudent);
+
+// const hobbies = ["reading", "hiking" , "coding" ];
+// console.log (hobbies);
+// console.log(hobbies[0]);
+// console.log(hobbies[2]);
+// hobbies.push("swimming" , "driving");
+// console.log(hobbies);
+// hobbies.shift();
+// console.log(hobbies); 
+
+// const person = {
+//     name : "John Doe" ,
+//     age : "30" ,
+//     isStudent : true,
+// };
+// console.log(name);
+// console.log(age);
+// console.log(isStudent);
+
+// console.log (person.occupation = "Programer");
+// console.log(person); 
+
+//  name = "John Doe";
+//  age = "30" ;
+// isStudent = true;
+
+if (age > 18) {
+    console.log("Adult");
+} else {
+    console.log("Not Adult");
+}
+
+
+// if (isStudent = true ) {
+//     console.log("Programer");
+// } else {
+//     console.log("shopkeeper");
+// }
+
+// if (hobbies.includes ("coding")) {
+//     console.log ("Programer")
+
+// } else {
+//     console.log("pro promgramer");
+// }
+function greet (name) {
+    console.log("Assalam e Alikum" , name );
+}  
+
+console.log(greet("Jon Dee"));
+
+function isAdult (age) {
+    if (age > 18) {
+        console.log("Adult");
+    } else {
+        console.log("Not Adult");
+    }
+
+}  
